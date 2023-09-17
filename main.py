7ae26fad664f3ba5f8db7f77c35185dd905228dd
Duaa Mjjo <75542436+DuaaMjjo@users.noreply.github.com>
2023-09-17 02:49:22 -0700

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import uvicorn
 
 if __name__ =="__main__":
+    nltk.download('punkt')
+    nltk.download('stopwords')
+    nltk.download('wordnet')
     uvicorn.run("app:app",host="0.0.0.0", port=10000, reload=True)
 
