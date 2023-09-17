633140b702f447334a7c38f5e2795cf704f5cac0
Duaa Mjjo <75542436+DuaaMjjo@users.noreply.github.com>
2023-09-17 00:36:52 -0700

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import uvicorn
 
 if __name__ =="__main__":
-    uvicorn.run("app:app", port=8000, reload=True)
+    uvicorn.run("app:app", port=10000, reload=True)
 
-#Run the API with uvicorn ,will run on local host
